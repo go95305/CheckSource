6a41f3d7fe3fc7f8a96b7914927ba81fb44ec489
NamWoojin <nam990304@gmail.com>
2021-04-20 20:41:59 +0900

    [frontend] fix : navbar 위치 변경
    App.vue에서 Login, Navbar를 포함한 다른 페이지를 보여주기 위해 변경

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,15 @@
 <template>
   <v-app>
-    <navbar></navbar>
-    <v-main id="main">
       <router-view></router-view>
-    </v-main>
   </v-app>
 </template>
 
 <script>
-import "@/assets/css/App.css"
-import Navbar from './views/Navbar.vue';
 
 export default {
   name: "App",
 
   components: {
-    Navbar,
   },
 
   data: () => ({
