0e90cfdbbb9065177be7421522d79d44b38c6771
NamWoojin <nam990304@gmail.com>
2021-04-21 17:06:06 +0900

    [frontend] feat : 현재 페이지에 따른 하이라이트 효과 추가

--- frontend/src/views/Main/Navbar.vue
+++ frontend/src/views/Main/Navbar.vue
@@ -1,11 +1,17 @@
 <template>
   <nav class="sidenav">
     <profile id="nav-profile" />
-    <router-link class="title-font" to="/dashboard">대시보드</router-link>
-    <router-link to="/dashboard">마이페이지</router-link>
-    <router-link to="/dashboard">전체목록</router-link>
+    <router-link :class="{ sidenav_choiced: currentPage == 1 }" to="/dashboard"
+      >대시보드</router-link
+    >
+    <router-link :class="{ sidenav_choiced: currentPage == 2 }" to="/dashboard"
+      >마이페이지</router-link
+    >
+    <router-link :class="{ sidenav_choiced: currentPage == 3 }" to="/dashboard"
+      >전체목록</router-link
+    >
     <div id="nav-logo">
-        <img src="http://placehold.it/100x100" alt="nav-logo" />
+      <img src="@/assets/shinhan.png" width="150px" alt="nav-logo" />
     </div>
   </nav>
 </template>
@@ -15,6 +21,24 @@ import Profile from "@/components/Main/Profile";
 export default {
   name: "Navbar",
   components: { Profile },
+  data() {
+    return {
+      currentPage: 0,
+      currentPathName: "",
+    };
+  },
+  watch: {
+    currentPathName: function () {
+      switch (this.currentPathName) {
+        case "DashBoard":
+          this.currentPage = 1;
+          break;
+      }
+    },
+  },
+  created() {
+    this.currentPathName = this.$route.name;
+  },
 };
 </script>
 
