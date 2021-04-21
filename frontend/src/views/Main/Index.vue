0563aa60d372a3e0579da3b344281e69e05bca89
NamWoojin <nam990304@gmail.com>
2021-04-21 10:41:18 +0900

    [frontend] feat: navbar 배치

---
+++ frontend/src/views/Main/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <router-view id="main"></router-view>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/views/Main/Navbar.vue';
+
+export default {
+   name: "Index",
+  components: { Navbar },
+};
+</script>
+<style scoped src="@/assets/css/Main/Index.css">
+</style>
