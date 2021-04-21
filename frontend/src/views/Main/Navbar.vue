833d20796d40d9227ac9790c8801f55f88451b03
NamWoojin <nam990304@gmail.com>
2021-04-21 16:40:38 +0900

    [frontend] feat : navbar 레이아웃 구현

--- frontend/src/views/Main/Navbar.vue
+++ frontend/src/views/Main/Navbar.vue
@@ -1,15 +1,20 @@
 <template>
   <nav class="sidenav">
+    <profile id="nav-profile" />
     <router-link class="title-font" to="/dashboard">대시보드</router-link>
     <router-link to="/dashboard">마이페이지</router-link>
     <router-link to="/dashboard">전체목록</router-link>
-</nav>
+    <div id="nav-logo">
+        <img src="http://placehold.it/100x100" alt="nav-logo" />
+    </div>
+  </nav>
 </template>
 
 <script>
+import Profile from "@/components/Main/Profile";
 export default {
   name: "Navbar",
-  components: {},
+  components: { Profile },
 };
 </script>
 
