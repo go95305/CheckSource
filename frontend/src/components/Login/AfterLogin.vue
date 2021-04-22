5875aa5d9f00a3e05d11d4d4c0d86b9acdfb81dc
NamWoojin <nam990304@gmail.com>
2021-04-22 10:27:41 +0900

    [frontend] fix : 로그인 화면 중앙 배치 수정

--- frontend/src/components/Login/AfterLogin.vue
+++ frontend/src/components/Login/AfterLogin.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="background">
-    <div class="page-container">
-      <div class="login-form-container shadow">
-        <div class="login-form-right-side-after">
-          <div class="top-logo-wrap"></div>
-          <!-- <img id="right-side-logo" src="@/assets/shinhan.png" /> -->
-          <h2>{{ MyInfo.EmployeeNumber }}님 반갑습니다.</h2>
-          <p>처음 사용하시는 사원은 개인 정보를 입력해주세요.</p>
+    <div class="login-form-container shadow">
+      <div class="login-form-right-side-after">
+        <div class="top-logo-wrap"></div>
+        <!-- <img id="right-side-logo" src="@/assets/shinhan.png" /> -->
+        <h2>{{ MyInfo.EmployeeNumber }}님 반갑습니다.</h2>
+        <p>처음 사용하시는 사원은 개인 정보를 입력해주세요.</p>
+      </div>
+      <div class="login-form-left-side">
+        <div class="login-top-wrap"></div>
+        <div class="login-input-container">
+          <div class="login-input-wrap input-name">
+            <i class="far fa-envelope"></i>
+            <input placeholder="성명" type="text" />
+          </div>
+          <div class="login-input-wrap input-department">
+            <i class="fas fa-key"></i>
+            <input placeholder="부서" type="text" />
+          </div>
+          <div class="login-input-wrap input-rank">
+            <i class="fas fa-key"></i>
+            <input placeholder="계급" type="text" />
+          </div>
         </div>
-        <div class="login-form-left-side">
-          <div class="login-top-wrap"></div>
-          <div class="login-input-container">
-            <div class="login-input-wrap input-name">
-              <i class="far fa-envelope"></i>
-              <input placeholder="성명" type="text" />
-            </div>
-            <div class="login-input-wrap input-department">
-              <i class="fas fa-key"></i>
-              <input placeholder="부서" type="text" />
-            </div>
-            <div class="login-input-wrap input-rank">
-              <i class="fas fa-key"></i>
-              <input placeholder="계급" type="text" />
-            </div>
-          </div>
-          <div class="login-btn-wrap">
-            <button class="login-btn">등록</button>
-          </div>
+        <div class="login-btn-wrap">
+          <button class="login-btn">등록</button>
         </div>
       </div>
     </div>
@@ -50,6 +48,4 @@ export default {
   },
 };
 </script>
-<style scoped src="../../assets/css/Main/Login.css">
-
-</style>
+<style scoped src="../../assets/css/Login/Login.css"></style>
--- frontend/src/views/Login/Login.vue
+++ frontend/src/views/Login/Login.vue
@@ -1,36 +1,34 @@
 <template>
   <div class="background">
-    <div class="page-container">
-      <div class="login-form-container shadow">
-        <div class="login-form-right-side-before">
-          <div class="top-logo-wrap"></div>
-          <img id="right-side-logo" src="@/assets/shinhan.png" />
-          <h3>Shinhan Bank Opensource Validation</h3>
+    <div class="login-form-container shadow">
+      <div class="login-form-right-side-before">
+        <div class="top-logo-wrap"></div>
+        <img id="right-side-logo" src="@/assets/shinhan.png" />
+        <h3>Shinhan Bank Opensource Validation</h3>
+      </div>
+      <div class="login-form-left-side">
+        <div class="login-top-wrap"></div>
+        <div class="login-input-container">
+          <div class="login-input-wrap input-id">
+            <i class="far fa-envelope"></i>
+            <input
+              v-model="loginInfo.EmployeeNumber"
+              placeholder="사번"
+              type="text"
+            />
+          </div>
+          <div class="login-input-wrap input-password">
+            <i class="fas fa-key"></i>
+            <input
+              v-model="loginInfo.Password"
+              placeholder="비밀번호"
+              type="password"
+            />
+          </div>
+          <p v-if="!isLogin">로그인 정보가 틀렸습니다. 다시 입력해주세요.</p>
         </div>
-        <div class="login-form-left-side">
-          <div class="login-top-wrap"></div>
-          <div class="login-input-container">
-            <div class="login-input-wrap input-id">
-              <i class="far fa-envelope"></i>
-              <input
-                v-model="loginInfo.EmployeeNumber"
-                placeholder="사번"
-                type="text"
-              />
-            </div>
-            <div class="login-input-wrap input-password">
-              <i class="fas fa-key"></i>
-              <input
-                v-model="loginInfo.Password"
-                placeholder="비밀번호"
-                type="password"
-              />
-            </div>
-            <p v-if="!isLogin">로그인 정보가 틀렸습니다. 다시 입력해주세요.</p>
-          </div>
-          <div class="login-btn-wrap">
-            <button class="login-btn" @click="showInfo">Login</button>
-          </div>
+        <div class="login-btn-wrap">
+          <button class="login-btn" @click="showInfo">Login</button>
         </div>
       </div>
     </div>
@@ -61,4 +59,4 @@ export default {
   },
 };
 </script>
-<style scoped src="../../assets/css/Main/Login.css"></style>
+<style scoped src="../../assets/css/Login/Login.css"></style>
