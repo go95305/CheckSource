df60658ce945ce70040e4bf18e7afb33afd8bd8a
KYC <go95305@naver.com>
2021-04-29 21:18:21 +0900

    [frontend] feat : 내 프로젝트 Component탭 테이블

---
+++ frontend/src/components/MyProject/OpensourceList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="table-header">
+      <div class="title title-1">오픈소스명</div>
+      <div class="title title-2">주소</div>
+      <div class="title title-3">라이선스</div>
+    </div>
+    <ul class="responsive-table" :key="index" v-for="(item, index) in list">
+      <li class="table-row">
+        <div class="col col-1">{{ item.name }}</div>
+        <div class="col col-2">{{ item.url }}</div>
+        <div class="col col-3">{{ item.obligation }}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      list: [
+        {
+          name: "Apache License 2.0",
+          url: "http://www.apache.org/licenses/LICENSE-2.0",
+          obligation: "Apache License 2.0",
+        },
+        {
+          name: "GNU General Public License v2.0 or later",
+          url: "https://opensource.org/licenses/BSD-3-Clause",
+          obligation: "Apache License 2.0",
+        },
+        {
+          name: "Eclipse Public License 2.0",
+          url: "https://www.eclipse.org/legal/epl-2.0",
+          obligation: "Apache License 2.0",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped src="../../assets/css/MyProject/LicenseList.css"></style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -19,121 +19,121 @@ import MyPage from "@/views/MyPage/MyPage";
 import MyPageProfile from "@/views/MyPage/MyPageProfile";
 import MyPageSCM from "@/views/MyPage/MyPageSCM";
 import MyPageGitLab from "@/views/MyPage/MyPageGitLab";
-
+import OpensourceList from "@/components/MyProject/OpensourceList";
 Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: "/",
-        name: "Login",
-        component: Login,
+  {
+    path: "/",
+    name: "Login",
+    component: Login,
+    children: [
+      {
+        path: "afterLogin",
+        name: "AfterLogin",
+        component: AfterLogin,
+      },
+      {
+        path: "",
+        name: "BeforeLogin",
+        component: BeforeLogin,
+      },
+    ],
+  },
+  {
+    path: "/",
+    name: "Index",
+    component: Index,
+    children: [
+      {
+        path: "dashboard",
+        name: "DashBoard",
+        component: DashBoard,
+      },
+      {
+        path: "project",
+        name: "Project",
+        component: MyProject,
         children: [
-            {
-                path: "afterLogin",
-                name: "AfterLogin",
-                component: AfterLogin,
-            },
-            {
-                path: "",
-                name: "BeforeLogin",
-                component: BeforeLogin,
-            },
-        ],
-    },
-    {
-        path: "/",
-        name: "Index",
-        component: Index,
-        children: [
-            {
-                path: "dashboard",
-                name: "DashBoard",
+          {
+            path: "main",
+            name: "MyProjectMain",
+            component: MyProjectMain,
+            children: [
+              {
+                path: "status",
+                name: "Status",
+                component: MyProjectStatus,
+              },
+              {
+                path: "gitlab",
+                name: "GitLab",
                 component: DashBoard,
-            },
-            {
-                path: "project",
-                name: "Project",
-                component: MyProject,
-                children: [
-                    {
-                        path: "main",
-                        name: "MyProjectMain",
-                        component: MyProjectMain,
-                        children: [
-                            {
-                                path: "status",
-                                name: "Status",
-                                component: MyProjectStatus,
-                            },
-                            {
-                                path: "gitlab",
-                                name: "GitLab",
-                                component: DashBoard,
-                            },
-                            {
-                                path: "github",
-                                name: "GitHub",
-                                component: DashBoard,
-                            },
-                        ],
-                    },
-                    {
-                        path: "result",
-                        name: "MyProjectResult",
-                        component: MyProjectResult,
-                        children: [
-                            {
-                                path: "summary",
-                                name: "Summary",
-                                component: DashBoard,
-                            },
-                            {
-                                path: "component",
-                                name: "Component",
-                                component: DashBoard,
-                            },
-                            {
-                                path: "license",
-                                name: "License",
-                                component: LicenseList,
-                            },
-                        ],
-                    },
-                ],
-            },
-            {
-                path: "mypage",
-                name: "MyPage",
-                component: MyPage,
-                children: [
-                    {
-                        path: "profile",
-                        name: "MyPageProfile",
-                        component: MyPageProfile,
-                    },
-                    {
-                        path: "scm",
-                        name: "MyPageSCM",
-                        component: MyPageSCM,
-                        children:[
-                            {
-                                path: "gitlab",
-                                name: "MyPageGitLab",
-                                component: MyPageGitLab,
-                            },
-                        ],
-                    },
-                ],
-            },
+              },
+              {
+                path: "github",
+                name: "GitHub",
+                component: DashBoard,
+              },
+            ],
+          },
+          {
+            path: "result",
+            name: "MyProjectResult",
+            component: MyProjectResult,
+            children: [
+              {
+                path: "summary",
+                name: "Summary",
+                component: DashBoard,
+              },
+              {
+                path: "component",
+                name: "Component",
+                component: OpensourceList,
+              },
+              {
+                path: "license",
+                name: "License",
+                component: LicenseList,
+              },
+            ],
+          },
         ],
-    },
+      },
+      {
+        path: "mypage",
+        name: "MyPage",
+        component: MyPage,
+        children: [
+          {
+            path: "profile",
+            name: "MyPageProfile",
+            component: MyPageProfile,
+          },
+          {
+            path: "scm",
+            name: "MyPageSCM",
+            component: MyPageSCM,
+            children: [
+              {
+                path: "gitlab",
+                name: "MyPageGitLab",
+                component: MyPageGitLab,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 const router = new VueRouter({
-    mode: "history",
-    base: process.env.BASE_URL,
-    linkActiveClass: "active",
-    routes,
+  mode: "history",
+  base: process.env.BASE_URL,
+  linkActiveClass: "active",
+  routes,
 });
 
 export default router;
