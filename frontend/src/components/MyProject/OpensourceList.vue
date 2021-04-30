3fdc9aecac7ea7692b753ac6d511e84596e8ed8d
koyuchang <go95305@naver.com>
2021-04-30 16:07:50 +0900

    [frontend] refactor : sideBar 오른쪽에 sideBar 나오도록 css 수정

--- frontend/src/components/MyProject/OpensourceList.vue
+++ frontend/src/components/MyProject/OpensourceList.vue
@@ -3,13 +3,18 @@
     <div class="mapped-component">
       <p>mapped component</p>
     </div>
-    <div class="mapped-table-header">
+    <div class="mapped-table-header" :class="{ sideBar: containerWidth }">
       <div class="title title-1">오픈소스명</div>
       <div class="title title-2">주소</div>
       <div class="title title-3">라이선스</div>
     </div>
-    <ul class="responsive-table" :key="index" v-for="(item, index) in list">
-      <li class="table-row">
+    <ul
+      class="responsive-table"
+      :key="`${index}_mapped`"
+      v-for="(item, index) in list"
+      :class="{ sideBar: containerWidth }"
+    >
+      <li class="table-row" @click="sidebar(item.version)">
         <div class="col col-1">{{ item.name }}</div>
         <div class="col col-2">{{ item.url }}</div>
         <div class="col col-3">{{ item.obligation }}</div>
@@ -19,39 +24,52 @@
       <p>unmapped component</p>
     </div>
     <button class="add-component">추가</button>
-    <div class="unmapped-table-header">
+    <div class="unmapped-table-header" :class="{ sideBar: containerWidth }">
       <div class="title title-1">Dependency</div>
       <div class="title title-2">위치</div>
     </div>
-    <ul class="responsive-table" :key="index" v-for="(item, index) in unmappedList">
+    <ul
+      class="responsive-table"
+      :key="`${idx}_unmapped`"
+      v-for="(item, idx) in unmappedList"
+      :class="{ sideBar: containerWidth }"
+    >
       <li class="table-row">
         <div class="col col-1">{{ item.name }}</div>
         <div class="col col-2">{{ item.origin }}</div>
       </li>
     </ul>
-    
+    <div class="component-specific" v-if="sidebarShow">
+      <a href="#" class="closebtn" @click="closeSideBar">&times;</a>
+      <a href="#">{{version}}</a>
+    </div>
   </div>
-  
 </template>
 <script>
 export default {
   data() {
     return {
+      sidebarShow: false,
+      containerWidth: false,
+      version: '',
       list: [
         {
           name: "Apache License 2.0",
           url: "http://www.apache.org/licenses/LICENSE-2.0",
           obligation: "Apache License 2.0",
+          version: "apache.0.1",
         },
         {
           name: "GNU General Public License v2.0 or later",
           url: "https://opensource.org/licenses/BSD-3-Clause",
           obligation: "Apache License 2.0",
+          version: "apache.2",
         },
         {
           name: "Eclipse Public License 2.0",
           url: "https://www.eclipse.org/legal/epl-2.0",
           obligation: "Apache License 2.0",
+          version: "axios.0.1",
         },
       ],
       unmappedList: [
@@ -70,6 +88,17 @@ export default {
       ],
     };
   },
+  methods: {
+    sidebar(versions) {
+      this.sidebarShow = true;
+      this.containerWidth = true;
+      this.version = versions;
+    },
+    closeSideBar() {
+      this.sidebarShow = false;
+      this.containerWidth = false;
+    },
+  },
 };
 </script>
-<style scoped src="../../assets/css/MyProject/LicenseList.css"></style>
+<style scoped src="../../assets/css/MyProject/OpensourceList.css"></style>
