a59f522a61ad6b60af64d543f37d2b2700264b45
KYC <go95305@naver.com>
2021-04-30 11:06:08 +0900

    [frontend] refactor : unmapped list 추가 1. mapped , unmapped component 분리하여 테이블 표시 2. LicenseList.css 수정

--- frontend/src/components/MyProject/OpensourceList.vue
+++ frontend/src/components/MyProject/OpensourceList.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
-    <div class="table-header">
+    <div class="mapped-component">
+      <p>mapped component</p>
+    </div>
+    <div class="mapped-table-header">
       <div class="title title-1">오픈소스명</div>
       <div class="title title-2">주소</div>
       <div class="title title-3">라이선스</div>
@@ -12,7 +15,23 @@
         <div class="col col-3">{{ item.obligation }}</div>
       </li>
     </ul>
+    <div class="unmapped-component">
+      <p>unmapped component</p>
+    </div>
+    <button class="add-component">추가</button>
+    <div class="unmapped-table-header">
+      <div class="title title-1">Dependency</div>
+      <div class="title title-2">위치</div>
+    </div>
+    <ul class="responsive-table" :key="index" v-for="(item, index) in unmappedList">
+      <li class="table-row">
+        <div class="col col-1">{{ item.name }}</div>
+        <div class="col col-2">{{ item.origin }}</div>
+      </li>
+    </ul>
+    
   </div>
+  
 </template>
 <script>
 export default {
@@ -33,6 +52,20 @@ export default {
           name: "Eclipse Public License 2.0",
           url: "https://www.eclipse.org/legal/epl-2.0",
           obligation: "Apache License 2.0",
+        },
+      ],
+      unmappedList: [
+        {
+          name: "Apache License 2.0",
+          origin: "http://www.apache.org/licenses/LICENSE-2.0",
+        },
+        {
+          name: "GNU General Public License v2.0 or later",
+          origin: "https://opensource.org/licenses/BSD-3-Clause",
+        },
+        {
+          name: "Eclipse Public License 2.0",
+          origin: "https://www.eclipse.org/legal/epl-2.0",
         },
       ],
     };
