849d1346420b179cafedbc201172e982e2ca7727
dahui <ekgml3765@naver.com>
2021-05-02 17:03:01 +0900

    [backend] test : CICD 테스트
    [backend] test : CICD 테스트

--- backend/checksource/src/main/java/com/ssafy/checksource/config/SwaggerConfig.java
+++ backend/checksource/src/main/java/com/ssafy/checksource/config/SwaggerConfig.java
@@ -26,7 +26,7 @@ public class SwaggerConfig {
         return new ApiInfoBuilder()
                 .title("Check Source API Documnentation")
                 .description(
-                        "Check Source API 문서입니다!\n\n백엔드 담당자 : 황다희,윤승환 \n 프론트 담당자 : 남우진,고유창,박은수 \n\n")
+                        "Check Source API 문서입니다!\n\n백엔드 담당자 :  황다희,윤승환 \n 프론트 담당자 : 남우진,고유창,박은수 \n\n")
                 .version("0.2")
                 .build();
     }
