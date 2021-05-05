638daac17024c4a17d69b2d97355d504045c8880
NamWoojin <nam990304@gmail.com>
2021-05-05 14:59:07 +0900

    [frontend] feat : 네비게이션 가드 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -194,4 +194,13 @@ const router = new VueRouter({
 	routes,
 });
 
+// router.beforeEach((to, from, next) => {
+// 	if (to.path != "/") {
+// 		if (!localStorage.getItem("token")) {
+// 			next("/");
+// 		}
+// 	}
+// 	next();
+// });
+
 export default router;
