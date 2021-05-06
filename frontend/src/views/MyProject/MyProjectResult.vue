20875c57b5f762b27fcc69af9dbbbdbbc983698d
NamWoojin <nam990304@gmail.com>
2021-05-06 20:19:19 +0900

    [frontend] fix : result tab 수정

--- frontend/src/views/MyProject/MyProjectResult.vue
+++ frontend/src/views/MyProject/MyProjectResult.vue
@@ -1,39 +1,39 @@
 <template>
-    <div>
-        <my-project-path :department="'내 프로젝트'" :project="projectId" />
-        <h1 id="my-project-result-title">{{ projectId }}</h1>
-        <tab id="my-project-result-tab" :list="tabList" />
-        <router-view />
-    </div>
+	<div>
+		<my-project-path :department="'내 프로젝트'" :project="projectId" />
+		<h1 id="my-project-result-title">{{ projectId }}</h1>
+		<tab id="my-project-result-tab" :list="tabList" />
+		<router-view />
+	</div>
 </template>
 <script>
 import MyProjectPath from "../../components/MyProject/MyProjectPath.vue";
 import Tab from "../../components/Tab/Tab.vue";
 export default {
-    name: "MyProjectResult",
-    components: { MyProjectPath, Tab },
-    data() {
-        return {
-            projectId: this.$route.query.projectId,
-            tabList: [],
-        };
-    },
-    created() {
-        this.tabList = [
-            {
-                name: "Summary",
-                path: "/project/result/summary?projectId=" + this.projectId,
-            },
-            {
-                name: "Component",
-                path: "/project/result/component?projectId=" + this.projectId,
-            },
-            {
-                name: "License",
-                path: "/project/result/license?projectId=" + this.projectId,
-            },
-        ];
-    },
+	name: "MyProjectResult",
+	components: { MyProjectPath, Tab },
+	data() {
+		return {
+			projectId: this.$route.query.projectId,
+			tabList: [],
+		};
+	},
+	created() {
+		this.tabList = [
+			{
+				name: "Summary",
+				path: "/project/result/summary",
+			},
+			{
+				name: "Component",
+				path: "/project/result/component",
+			},
+			{
+				name: "License",
+				path: "/project/result/license",
+			},
+		];
+	},
 };
 </script>
 <style scoped src="@/assets/css/MyProject/MyProjectResult.css"></style>
