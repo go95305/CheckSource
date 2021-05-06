9babbb4900e012b9d053e640695f5e248e04ed86
eunsu27 <pesu1027@gmail.com>
2021-05-06 17:13:46 +0900

    [frontend] fix : 라이선스 추가 label 수정

--- frontend/src/views/MyProject/MyProjectAddLicense.vue
+++ frontend/src/views/MyProject/MyProjectAddLicense.vue
@@ -1,15 +1,21 @@
 <template>
   <form action="">
-    <!--  General -->
     <div class="form-group">
       <h2 class="heading">라이선스 추가</h2>
       <div class="controls">
         <input type="text" id="name" class="floatLabel" name="name" />
-        <label for="name">Name</label>
+        <label class="form-control-placeholder" for="name">Full name</label>
       </div>
       <div class="controls">
-        <input type="email" id="email" class="floatLabel" name="email" />
-        <label for="email">Email</label>
+        <input
+          type="text"
+          id="identifier"
+          class="floatLabel"
+          name="identifier"
+        />
+        <label class="form-control-placeholder" for="identifier"
+          >Identifier</label
+        >
       </div>
       <div class="controls">
         <i class="fa fa-sort"></i>
@@ -21,11 +27,11 @@
           <option value="Orange">Orange</option>
           <option value="Watermelon">Watermelon</option>
         </select>
-        <label for="fruit">Favotire Fruit</label>
+        <label class="form-control-placeholder" for="fruit">Risk</label>
       </div>
       <div class="controls">
-        <textarea name="comments" class="floatLabel" id="comments"></textarea>
-        <label for="comments">Comments</label>
+        <textarea name="notice" class="floatLabel" id="notice"></textarea>
+        <label class="form-control-placeholder" for="notice">Notice</label>
         <button>Submit</button>
       </div>
     </div>
