219fb17187c83c6b480d89d5881153ba151d9a3f
NamWoojin <nam990304@gmail.com>
2021-05-06 17:10:46 +0900

    [frontend] fix : 라우터 오류 해결

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -29,291 +29,178 @@ import MyPageSCM from "@/views/MyPage/MyPageSCM";
 import MyPageGitLab from "@/views/MyPage/MyPageGitLab";
 import OpensourceList from "@/components/MyProject/OpensourceList";
 import AddComponent from "@/components/MyProject/AddComponent";
-import OpensourceMain from "@/views/MyProject/OpensourceMain"
+import OpensourceMain from "@/views/MyProject/OpensourceMain";
 Vue.use(VueRouter);
 
 const routes = [
-<<<<<<< HEAD
-  {
-    path: "/",
-    name: "Login",
-    component: Login,
-    children: [
-      {
-        path: "afterLogin",
-        name: "AfterLogin",
-        component: AfterLogin,
-      },
-      {
-        path: "",
-        name: "BeforeLogin",
-        component: BeforeLogin,
-      },
-    ],
-  },
-  {
-    path: "/",
-    name: "Index",
-    component: Index,
-    children: [
-      {
-        path: "dashboard",
-        name: "DashBoard",
-        component: DashBoard,
-      },
-      {
-        path: "project",
-        name: "Project",
-        component: MyProject,
+    {
+        path: "/",
+        name: "Login",
+        component: Login,
         children: [
-          {
-            path: "main",
-            name: "MyProjectMain",
-            component: MyProjectMain,
-            children: [
-              {
-                path: "status",
-                name: "Status",
-                component: MyProjectStatus,
-              },
-              {
-                path: "gitlab",
-                name: "GitLab",
+            {
+                path: "afterLogin",
+                name: "AfterLogin",
+                component: AfterLogin,
+            },
+            {
+                path: "",
+                name: "BeforeLogin",
+                component: BeforeLogin,
+            },
+        ],
+    },
+    {
+        path: "/",
+        name: "Index",
+        component: Index,
+        children: [
+            {
+                path: "dashboard",
+                name: "DashBoard",
                 component: DashBoard,
-              },
-              {
-                path: "github",
-                name: "GitHub",
-                component: DashBoard,
-              },
-            ],
-          },
-          {
-            path: "result",
-            name: "MyProjectResult",
-            component: MyProjectResult,
-            children: [
-              {
-                path: "summary",
-                name: "Summary",
-                component: DashBoard,
-              },
-
-              {
-                path: "license",
-                name: "License",
-                component: LicenseList,
-              },
-              {
-                path: "component",
-                name: "Component",
-                component: OpensourceMain,
+            },
+            {
+                path: "project",
+                name: "Project",
+                component: MyProject,
                 children: [
-                  {
-                    path: "addComponent",
-                    name: "AddComponent",
-                    component: AddComponent,
-                  },
-                  {
-                    path: "",
-                    name: "ComponentList",
-                    component: OpensourceList,
-                  },
+                    {
+                        path: "main",
+                        name: "MyProjectMain",
+                        component: MyProjectMain,
+                        children: [
+                            {
+                                path: "status",
+                                name: "Status",
+                                component: MyProjectStatus,
+                            },
+                            {
+                                path: "gitlab",
+                                name: "GitLab",
+                                component: MyProjectGitlab,
+                            },
+                            {
+                                path: "github",
+                                name: "GitHub",
+                                component: DashBoard,
+                            },
+                            {
+                                path: "edit",
+                                name: "MyProjectEdit",
+                                component: MyProjectEdit,
+                                children: [
+                                    {
+                                        path: "",
+                                        redirect: "gitlab",
+                                    },
+                                    {
+                                        path: "gitlab",
+                                        name: "MyProjectEditGitLab",
+                                        component: MyProjectEditGitLab,
+                                        props: true,
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                    {
+                        path: "result",
+                        name: "MyProjectResult",
+                        component: MyProjectResult,
+                        children: [
+                            {
+                                path: "summary",
+                                name: "Summary",
+                                component: DashBoard,
+                            },
+                            {
+                                path: "license",
+                                name: "License",
+                                component: LicenseList,
+                            },
+                            {
+                                path: "component",
+                                name: "Component",
+                                component: OpensourceMain,
+                                children: [
+                                    {
+                                        path: "addComponent",
+                                        name: "AddComponent",
+                                        component: AddComponent,
+                                    },
+                                    {
+                                        path: "",
+                                        name: "ComponentList",
+                                        component: OpensourceList,
+                                    },
+                                ],
+                            },
+                        ],
+                    },
                 ],
-              },
-            ],
-          },
+            },
+            {
+                path: "list",
+                name: "OSSMain",
+                component: OSSMain,
+                children: [
+                    {
+                        path: "",
+                        name: "OSSList",
+                        component: OSSList,
+                        children: [
+                            {
+                                path: "",
+                                redirect: "opensource",
+                            },
+                            {
+                                path: "opensource",
+                                name: "OSSOpenSource",
+                                component: OSSOpenSource,
+                            },
+                            {
+                                path: "license",
+                                name: "OSSLicense",
+                                component: OSSLicense,
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                path: "mypage",
+                name: "MyPage",
+                component: MyPage,
+                children: [
+                    {
+                        path: "profile",
+                        name: "MyPageProfile",
+                        component: MyPageProfile,
+                    },
+                    {
+                        path: "scm",
+                        name: "MyPageSCM",
+                        component: MyPageSCM,
+                        children: [
+                            {
+                                path: "gitlab",
+                                name: "MyPageGitLab",
+                                component: MyPageGitLab,
+                            },
+                        ],
+                    },
+                ],
+            },
         ],
-      },
-      {
-        path: "mypage",
-        name: "MyPage",
-        component: MyPage,
-        children: [
-          {
-            path: "profile",
-            name: "MyPageProfile",
-            component: MyPageProfile,
-          },
-          {
-            path: "scm",
-            name: "MyPageSCM",
-            component: MyPageSCM,
-            children: [
-              {
-                path: "gitlab",
-                name: "MyPageGitLab",
-                component: MyPageGitLab,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-=======
-	{
-		path: "/",
-		name: "Login",
-		component: Login,
-		children: [
-			{
-				path: "afterLogin",
-				name: "AfterLogin",
-				component: AfterLogin,
-			},
-			{
-				path: "",
-				name: "BeforeLogin",
-				component: BeforeLogin,
-			},
-		],
-	},
-	{
-		path: "/",
-		name: "Index",
-		component: Index,
-		children: [
-			{
-				path: "dashboard",
-				name: "DashBoard",
-				component: DashBoard,
-			},
-			{
-				path: "project",
-				name: "Project",
-				component: MyProject,
-				children: [
-					{
-						path: "main",
-						name: "MyProjectMain",
-						component: MyProjectMain,
-						children: [
-							{
-								path: "status",
-								name: "Status",
-								component: MyProjectStatus,
-							},
-							{
-								path: "gitlab",
-								name: "GitLab",
-								component: MyProjectGitlab,
-							},
-							{
-								path: "github",
-								name: "GitHub",
-								component: DashBoard,
-							},
-							{
-								path: "edit",
-								name: "MyProjectEdit",
-								component: MyProjectEdit,
-								children: [
-									{
-										path: "",
-										redirect: "gitlab",
-									},
-									{
-										path: "gitlab",
-										name: "MyProjectEditGitLab",
-										component: MyProjectEditGitLab,
-										props: true,
-									},
-								],
-							},
-						],
-					},
-					{
-						path: "result",
-						name: "MyProjectResult",
-						component: MyProjectResult,
-						children: [
-							{
-								path: "summary",
-								name: "Summary",
-								component: DashBoard,
-							},
-							{
-								path: "license",
-								name: "License",
-								component: LicenseList,
-							},
-							{
-								path: "component",
-								name: "Component",
-								component: OpensourceList,
-							},
-							{
-								path: "addComponent",
-								name: "AddComponent",
-								component: AddComponent,
-							},
-						],
-					},
-				],
-			},
-			{
-				path: "list",
-				name: "OSSMain",
-				component: OSSMain,
-				children: [
-					{
-						path: "",
-						name: "OSSList",
-						component: OSSList,
-						children: [
-							{
-								path: "",
-								redirect: "opensource",
-							},
-							{
-								path: "opensource",
-								name: "OSSOpenSource",
-								component: OSSOpenSource,
-							},
-							{
-								path: "license",
-								name: "OSSLicense",
-								component: OSSLicense,
-							},
-						],
-					},
-				],
-			},
-			{
-				path: "mypage",
-				name: "MyPage",
-				component: MyPage,
-				children: [
-					{
-						path: "profile",
-						name: "MyPageProfile",
-						component: MyPageProfile,
-					},
-					{
-						path: "scm",
-						name: "MyPageSCM",
-						component: MyPageSCM,
-						children: [
-							{
-								path: "gitlab",
-								name: "MyPageGitLab",
-								component: MyPageGitLab,
-							},
-						],
-					},
-				],
-			},
-		],
-	},
->>>>>>> frontend
+    },
 ];
 
 const router = new VueRouter({
-	mode: "history",
-	base: process.env.BASE_URL,
-	// linkActiveClass: "active",
-	routes,
+    mode: "history",
+    base: process.env.BASE_URL,
+    // linkActiveClass: "active",
+    routes,
 });
 
 export default router;
