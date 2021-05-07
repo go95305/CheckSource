c39f48de775d1172d48dcc7076776c5de1bd2f1d
KYC <go95305@naver.com>
2021-05-07 09:48:51 +0900

    [frontend] refactor : 상세보기 상세보기 위치 변경

--- frontend/src/components/MyProject/OpensourceList.vue
+++ frontend/src/components/MyProject/OpensourceList.vue
@@ -3,7 +3,7 @@
     <div class="mapped-component">
       <p>mapped component</p>
     </div>
-    <div class="mapped-table-header" :class="{ sideBar: containerWidth }">
+    <div class="mapped-table-header" >
       <div class="title title-1">오픈소스명</div>
       <div class="title title-2">주소</div>
       <div class="title title-3">라이선스</div>
@@ -12,19 +12,26 @@
       class="responsive-table"
       :key="`${index}_mapped`"
       v-for="(item, index) in list"
-      :class="{ sideBar: containerWidth }"
     >
-      <li class="table-row" @click="sidebar(item.version)">
+      <li class="table-row" @click="sidebar">
         <div class="col col-1">{{ item.name }}</div>
         <div class="col col-2">{{ item.url }}</div>
         <div class="col col-3">{{ item.obligation }}</div>
       </li>
+
+        <div class="component-specific">
+            <p class="version">License Version: {{ item.version }}</p>
+            <p class="package-type">Packge Type: {{ item.packageType }}</p>
+            <p class="artifact-id">Artifact Id: {{ item.artifactId }}</p>
+            <p class="group-id">Group Id: {{ item.groupId }}</p>
+        </div>
+
     </ul>
     <div class="unmapped-component">
       <p>unmapped component</p>
     </div>
     <button class="add-component" @click="addComponent">Component 추가</button>
-    <div class="unmapped-table-header" :class="{ sideBar: containerWidth }">
+    <div class="unmapped-table-header">
       <div class="title utitle-1">Dependency</div>
       <div class="title utitle-2">위치</div>
     </div>
@@ -32,49 +39,51 @@
       class="responsive-table"
       :key="`${idx}_unmapped`"
       v-for="(item, idx) in unmappedList"
-      :class="{ sideBar: containerWidth }"
     >
       <li class="table-row">
         <div class="col ucol-1">{{ item.name }}</div>
         <div class="col ucol-2">{{ item.origin }}</div>
       </li>
     </ul>
-    <div class="component-specific" v-if="sidebarShow">
-      <div>
-        <button class="closebtn" @click="closeSideBar">&times;</button>
-      </div>
-      <div class="component-version">
-        <p>License Version</p>
-        <p class="version">{{ version }}</p>
-      </div>
-    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      sidebarShow: false,
+      sidebarShow: [],
       containerWidth: false,
       version: "",
+      packageType: "",
+      artifactId: "",
+      groupId: "",
       list: [
         {
           name: "Apache License 2.0",
           url: "http://www.apache.org/licenses/LICENSE-2.0",
           obligation: "Apache License 2.0",
           version: "apache.0.1",
+          packageType: "s",
+          artifactId: "a",
+          groupId: "g",
         },
         {
           name: "GNU General Public License v2.0 or later",
           url: "https://opensource.org/licenses/BSD-3-Clause",
           obligation: "Apache License 2.0",
           version: "apache.2",
+          packageType: "s",
+          artifactId: "a",
+          groupId: "g",
         },
         {
           name: "Eclipse Public License 2.0",
           url: "https://www.eclipse.org/legal/epl-2.0",
           obligation: "Apache License 2.0",
           version: "axios.0.1",
+          packageType: "s",
+          artifactId: "a",
+          groupId: "g",
         },
       ],
       unmappedList: [
@@ -94,17 +103,28 @@ export default {
     };
   },
   methods: {
-    sidebar(versions) {
-      this.sidebarShow = true;
-      this.containerWidth = true;
-      this.version = versions;
+    sidebar(event) {
+      if(event.target.classList.contains("choice")){
+        event.target.classList.remove("choice");
+      }else{
+        let dropbtns = this.$el.getElementsByClassName("choice");
+        for(let i =0;i<dropbtns.length;i++){
+          dropbtns[i].classList.remove("choice");
+        }
+        event.target.classList.add("choice");
+      }
+      // this.sidebarShow[index] = !this.sidebarShow[index];
+      // this.version = item.version;
+      // this.packageType = item.packageType;
+      // this.artifactId = item.artifactId;
+      // this.groupId = item.groupId;
     },
     closeSideBar() {
       this.sidebarShow = false;
       this.containerWidth = false;
     },
     addComponent() {
-      this.$router.push({name:"AddComponent"});
+      this.$router.push({ name: "AddComponent" });
     },
   },
 };
