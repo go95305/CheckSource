9187ee19698d307a3bf5c3594f22fdae5ae1cef5
NamWoojin <nam990304@gmail.com>
2021-05-09 18:12:04 +0900

    [frontend] feat : 오픈소스 상세페이지 구현

---
+++ frontend/src/components/OSS/OSSDetailOpenSourceTable.vue
@@ -0,0 +1,29 @@
+<template>
+    <table class="detail-table">
+        <tr class="detail-table-tr">
+            <td class="detail-table-td-head">NAME</td>
+            <td class="detail-table-td-body">이름</td>
+        </tr>
+        <tr class="detail-table-tr">
+            <td class="detail-table-td-head">LICENSE</td>
+            <td class="detail-table-td-body">라이선스</td>
+        </tr>
+        <tr class="detail-table-tr">
+            <td class="detail-table-td-head">COPYRIGHT</td>
+            <td class="detail-table-td-body">저작권</td>
+        </tr>
+        <tr class="detail-table-tr">
+            <td class="detail-table-td-head">URL</td>
+            <td class="detail-table-td-body">url</td>
+        </tr>
+    </table>
+</template>
+<script>
+import "@/assets/css/OSS/OSSDetailTable.css";
+export default {
+    name: "OSSDetailOpenSourceTable",
+    props: {
+        opensource: Object,
+    },
+};
+</script>
