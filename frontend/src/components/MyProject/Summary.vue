0a968c0e2277d3f6a2727d1a95b3a6cac5bd97ac
eunsu27 <pesu1027@gmail.com>
2021-05-10 02:31:10 +0900

    [frontend] feat : summary card layout

---
+++ frontend/src/components/MyProject/Summary.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="summary-container">
+    <div class="main__cards">
+      <!-- 컴포넌트 -->
+
+      <!-- 컴포넌트 end  -->
+
+      <!-- 라이선스 -->
+      <div class="sum-container">
+        <div class="summary-card">
+          <div class="img-container">
+            <div class="verifycard-aside-detail">
+              <span class="verifycard-difficulty">12</span><br />
+              <span class="verifycard-skill">Components</span>
+            </div>
+          </div>
+          <div class="card-content">
+            <h2>COMPONENT</h2>
+            <span>확인이 필요한 컴포넌트 </span>
+            <p class="excerpt">
+              Component mapping이 완료되지 않았습니다. '확인하기'를 눌러
+              mapping을 진행해 주세요
+            </p>
+            <p class="author">확인하기</p>
+          </div>
+        </div>
+      </div>
+      <!-- 라이선스 end  -->
+
+      <!-- 타임라인 -->
+
+      <!-- 타임라인 end -->
+    </div>
+  </div>
+</template>
+<script>
+import '@/assets/css/MyProject/Summary.scss';
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
