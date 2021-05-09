abd580acb35d79d4ced68437a4d9dd8c848e1c42
NamWoojin <nam990304@gmail.com>
2021-05-09 19:32:48 +0900

    [frontend] feat : 라이선스 상세페이지 라우터

--- frontend/src/views/OSS/OSSDetailLicense.vue
+++ frontend/src/views/OSS/OSSDetailLicense.vue
@@ -1,8 +1,38 @@
 <template>
-    <div></div>
+    <div>
+        <my-project-path
+            :department="'전체목록'"
+            :project="license.name"
+            rootPath="/list/license"
+        />
+        <h1 class="oss-detail-title">{{ license.name }}</h1>
+        <tab :list="tabList"></tab>
+        <router-view></router-view>
+    </div>
 </template>
 <script>
+import MyProjectPath from "@/components/MyProject/MyProjectPath.vue";
+import Tab from "@/components/Tab/Tab.vue";
 export default {
     name: "OSSDetailLicense",
+    components: {
+        MyProjectPath,
+        Tab,
+    },
+    data() {
+        return {
+            id: this.$route.query.id,
+            license: {
+                id: "",
+                name: "라이선스 이름",
+            },
+            tabList: [
+                {
+                    name: "Content",
+                    path: "/list/detail/license/content",
+                },
+            ],
+        };
+    },
 };
 </script>
