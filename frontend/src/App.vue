3b7becb82a414aca59f58c608797cfb5a7468892
NamWoojin <nam990304@gmail.com>
2021-05-11 13:14:59 +0900

    [frontend] feat : 로딩 제작

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,34 @@
 <template>
-    <div id="app">
-        <router-view />
-    </div>
+	<div id="app">
+		<div v-if="loading" class="loading">
+			<loading></loading>
+		</div>
+		<router-view />
+	</div>
 </template>
 <script>
+import Loading from "@/components/Loading/Loading";
 export default {
-    name: "App",
-    created() {
-        //자동로그인
-        const localStorageVuex = localStorage.getItem("vuex");
-        if (localStorageVuex) {
-            const userToken = JSON.parse(localStorageVuex).accessToken;
-            if (userToken != null) {
-                //현재 페이지가 로그인 페이지라면 대시보드로 이동.
-                if (this.$route.name == "BeforeLogin")
-                    this.$router.push("/dashboard");
-            }
-        }
-    },
+	name: "App",
+	data() {
+		return {
+			loading: false,
+		};
+	},
+	components: {
+		Loading,
+	},
+	created() {
+		//자동로그인
+		const localStorageVuex = localStorage.getItem("vuex");
+		if (localStorageVuex) {
+			const userToken = JSON.parse(localStorageVuex).accessToken;
+			if (userToken != null) {
+				//현재 페이지가 로그인 페이지라면 대시보드로 이동.
+				if (this.$route.name == "BeforeLogin") this.$router.push("/dashboard");
+			}
+		}
+	},
 };
 </script>
 <style>
@@ -25,10 +36,24 @@ export default {
 @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
 
 * {
-    margin: 0;
+	margin: 0;
 }
 
 #app {
-    font-family: "Noto Sans KR";
+	font-family: "Noto Sans KR";
+}
+
+.loading {
+	position: absolute;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	left: 0px;
+	height: 0px;
+	z-index: 10;
+	width: 100vw;
+	height: 100vh;
+	overflow: hidden;
+	background-color: rgba(128, 128, 128, 0.466);
 }
 </style>
