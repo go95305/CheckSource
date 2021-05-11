05cb5c8537b5dc0544b4fec74a44dc79c5beb3ad
eunsu27 <pesu1027@gmail.com>
2021-05-11 22:31:49 +0900

    [frontend] feat : 체크박스 추가

--- frontend/src/components/MyProject/MyProjectAddLicense.vue
+++ frontend/src/components/MyProject/MyProjectAddLicense.vue
@@ -62,16 +62,38 @@
       </p> -->
 
       <p class="field required">
-        <label class="label required" for="name">Package Type</label>
-        <input
-          class="text-input"
-          id="name"
-          name="name"
-          required
-          type="text"
-          placeholder="package type"
-        />
+        <label class="label required">소스코드 공개여부</label>
+        <ul class="unstyled centered">
+          <li class="add-l-check">
+            <input
+              class="styled-checkbox"
+              id="codeopen1"
+              type="checkbox"
+              value="co1"
+            />
+            <label for="codeopen1">모듈 단위 공개</label>
+          </li>
+          <li class="add-l-check">
+            <input
+              class="styled-checkbox"
+              id="codeopen2"
+              type="checkbox"
+              value="co2"
+            />
+            <label for="codeopen2">네트워크 서비스 포함 전체코드 공개</label>
+          </li>
+          <li class="add-l-check">
+            <input
+              class="styled-checkbox"
+              id="codeopen3"
+              type="checkbox"
+              value="co3"
+            />
+            <label for="codeopen3">표준버전</label>
+          </li>
+        </ul>
       </p>
+
     </form>
 
     <div class="box-3">
