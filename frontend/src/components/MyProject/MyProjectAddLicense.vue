156bac374d41bf965fd3f82e048c18c7014353fd
eunsu27 <pesu1027@gmail.com>
2021-05-11 22:08:04 +0900

    [frontend] feat : 라이선스추가 디자인통일 레이아웃
    [frontend] feat : 라이선스추가 디자인통일 레이아웃

--- frontend/src/components/MyProject/MyProjectAddLicense.vue
+++ frontend/src/components/MyProject/MyProjectAddLicense.vue
@@ -1,41 +1,85 @@
 <template>
-  <form action="">
-    <div class="form-group">
-      <h2 class="heading">라이선스 추가</h2>
-      <div class="controls">
-        <input type="text" id="name" class="floatLabel" name="name" />
-        <label class="form-control-placeholder" for="name">Full name</label>
-      </div>
-      <div class="controls">
+  <div class="form-section">
+    <div class="title">
+      <h2>새로운 License 추가</h2>
+    </div>
+    <form action="" class="form">
+      <p class="field required">
+        <label class="label required" for="lname">라이선스 이름</label>
         <input
+          class="text-input"
+          id="lname"
+          name="lname"
+          required
           type="text"
-          id="identifier"
-          class="floatLabel"
-          name="identifier"
+          placeholder="라이선스 이름"
         />
-        <label class="form-control-placeholder" for="identifier"
-          >Identifier</label
-        >
-      </div>
-      <div class="controls">
-        <i class="fa fa-sort"></i>
-        <select class="floatLabel">
-          <option value="blank"></option>
-          <option value="Apple">Apple</option>
-          <option value="Banana">Banana</option>
-          <option value="Kiwi">Kiwi</option>
-          <option value="Orange">Orange</option>
-          <option value="Watermelon">Watermelon</option>
-        </select>
-        <label class="form-control-placeholder" for="fruit">Risk</label>
-      </div>
-      <div class="controls">
-        <textarea name="notice" class="floatLabel" id="notice"></textarea>
-        <label class="form-control-placeholder" for="notice">Notice</label>
-        <button>Submit</button>
+      </p>
+      <p class="field required">
+        <label class="label" for="identifier">Identifier</label>
+        <input
+          class="text-input"
+          id="identifier"
+          name="identifier"
+          required
+          type="text"
+          placeholder="Identifier"
+        />
+      </p>
+      <p class="field required">
+        <label class="label" for="url">URL</label>
+        <input
+          class="text-input"
+          id="url"
+          name="url"
+          required
+          type="text"
+          placeholder="URL"
+        />
+      </p>
+      <p class="field required">
+        <label class="label required" for="contents">라이선스 전문</label>
+        <textarea
+          rows="8"
+          class="text-input"
+          id="contents"
+          name="contents"
+          required
+          type="text"
+          placeholder="라이선스 전문"
+        />
+      </p>
+      <!-- <p class="field required">
+        <label class="label required" for="name">Package Type</label>
+        <input
+          class="text-input"
+          id="name"
+          name="name"
+          required
+          type="text"
+          placeholder="package type"
+        />
+      </p> -->
+
+      <p class="field required">
+        <label class="label required" for="name">Package Type</label>
+        <input
+          class="text-input"
+          id="name"
+          name="name"
+          required
+          type="text"
+          placeholder="package type"
+        />
+      </p>
+    </form>
+
+    <div class="box-3">
+      <div class="btn btn-three">
+        <span>추가</span>
       </div>
     </div>
-  </form>
+  </div>
 </template>
 
 <script>
