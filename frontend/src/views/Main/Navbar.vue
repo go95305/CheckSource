5fca394d4f338f808008ef275ad4da8abff98170
NamWoojin <nam990304@gmail.com>
2021-05-11 16:58:26 +0900

    [frontend] fix : 프로젝트 목록 navbar, title 변경

--- frontend/src/views/Main/Navbar.vue
+++ frontend/src/views/Main/Navbar.vue
@@ -1,120 +1,102 @@
 <template>
-    <nav class="sidenav">
-        <profile id="nav-profile" />
-        <ul class="nav-ul">
-            <li class="nav-li" @click="MenuClickHandler">
-                <router-link to="/dashboard">대시보드</router-link>
-            </li>
-            <li class="nav-li">
-                <div class="dropdown">
-                    <span
-                        class="dropbtn"
-                        @click="MenuClickHandler"
-                        :class="{
-                            'router-link-active': currentPathName == 'project',
-                        }"
-                        >프로젝트</span
-                    >
-                    <div class="dropdown-content">
-                        <router-link to="/project/main/status"
-                            >Status</router-link
-                        >
-                        <router-link to="/project/main/gitlab"
-                            >GitLab</router-link
-                        >
-                        <!-- <router-link to="/project/main/github">GitHub</router-link> -->
-                    </div>
-                </div>
-            </li>
-            <li class="nav-li">
-                <div class="dropdown">
-                    <span
-                        class="dropbtn"
-                        @click="MenuClickHandler"
-                        :class="{
-                            'router-link-active': currentPathName == 'list',
-                        }"
-                        >전체목록</span
-                    >
-                    <div class="dropdown-content">
-                        <router-link to="/list/opensource"
-                            >Open Source</router-link
-                        >
-                        <router-link to="/list/license">License</router-link>
-                    </div>
-                </div>
-            </li>
-            <li class="nav-li">
-                <div class="dropdown">
-                    <span
-                        class="dropbtn"
-                        @click="MenuClickHandler"
-                        :class="{
-                            'router-link-active': currentPathName == 'mypage',
-                        }"
-                        >마이페이지</span
-                    >
-                    <div class="dropdown-content">
-                        <router-link to="/mypage/profile">Profile</router-link>
-                        <router-link to="/mypage/scm/gitlab">SCM</router-link>
-                    </div>
-                </div>
-            </li>
-        </ul>
+	<nav class="sidenav">
+		<profile id="nav-profile" />
+		<ul class="nav-ul">
+			<li class="nav-li" @click="MenuClickHandler">
+				<router-link to="/dashboard">대시보드</router-link>
+			</li>
+			<li class="nav-li">
+				<div class="dropdown">
+					<span
+						class="dropbtn"
+						@click="MenuClickHandler"
+						:class="{
+							'router-link-active': currentPathName == 'project',
+						}"
+						>프로젝트</span
+					>
+					<div class="dropdown-content">
+						<router-link to="/project/main/status">Status</router-link>
+						<router-link to="/project/main/projects">Projects</router-link>
+					</div>
+				</div>
+			</li>
+			<li class="nav-li">
+				<div class="dropdown">
+					<span
+						class="dropbtn"
+						@click="MenuClickHandler"
+						:class="{
+							'router-link-active': currentPathName == 'list',
+						}"
+						>전체목록</span
+					>
+					<div class="dropdown-content">
+						<router-link to="/list/opensource">Open Source</router-link>
+						<router-link to="/list/license">License</router-link>
+					</div>
+				</div>
+			</li>
+			<li class="nav-li">
+				<div class="dropdown">
+					<span
+						class="dropbtn"
+						@click="MenuClickHandler"
+						:class="{
+							'router-link-active': currentPathName == 'mypage',
+						}"
+						>마이페이지</span
+					>
+					<div class="dropdown-content">
+						<router-link to="/mypage/profile">Profile</router-link>
+						<router-link to="/mypage/scm/gitlab">SCM</router-link>
+					</div>
+				</div>
+			</li>
+		</ul>
 
-        <!-- <router-link
-            :class="{ sidenav_choiced: currentPathName == 'myproject' }"
-            to="/myproject/main/summary"
-            >내 프로젝트</router-link
-        >
-        <router-link
-            :class="{ sidenav_choiced: currentPathName == 'TotalList' }"
-            to="/dashboard"
-            >전체목록</router-link
-        > -->
-
-        <div id="nav-logo">
-            <img src="@/assets/shinhan.png" alt="nav-logo" />
-        </div>
-    </nav>
+		<div id="nav-logo">
+			<img src="@/assets/shinhan.png" alt="nav-logo" />
+		</div>
+	</nav>
 </template>
 
 <script>
 import Profile from "@/components/Main/Profile";
 export default {
-    name: "Navbar",
-    components: { Profile },
-    data() {
-        return {
-            currentPathName: "",
-        };
-    },
-    watch: {
-        $route() {
-            this.CheckRouter();
-        },
-    },
-    created() {
-        this.CheckRouter();
-    },
-    methods: {
-        MenuClickHandler: function (event) {
-            if (event.target.classList.contains("choice")) {
-                event.target.classList.remove("choice");
-            } else {
-                let dropbtns = this.$el.getElementsByClassName("choice");
-                for (let i = 0; i < dropbtns.length; i++) {
-                    dropbtns[i].classList.remove("choice");
-                }
-                event.target.classList.add("choice");
-            }
-        },
-        CheckRouter: function () {
-            //경로 이동 시 해당 경로 이름 설정
-            const path = this.$route.path.split("/");
-            this.currentPathName = path[1];
-        },
-    },
+	name: "Navbar",
+	components: { Profile },
+	data() {
+		return {
+			currentPathName: "",
+		};
+	},
+	watch: {
+		$route() {
+			this.CheckRouter();
+		},
+	},
+	created() {
+		this.CheckRouter();
+	},
+	methods: {
+		MenuClickHandler: function (event) {
+			if (event.target.classList.contains("choice")) {
+				event.target.classList.remove("choice");
+			} else {
+				let dropbtns = this.$el.getElementsByClassName("choice");
+				for (let i = 0; i < dropbtns.length; i++) {
+					dropbtns[i].classList.remove("choice");
+				}
+				event.target.classList.add("choice");
+			}
+		},
+		CheckRouter: function () {
+			//경로 이동 시 해당 경로 이름 설정
+			const path = this.$route.path.split("/");
+			this.currentPathName = path[1];
+		},
+	},
 };
 </script>
 
