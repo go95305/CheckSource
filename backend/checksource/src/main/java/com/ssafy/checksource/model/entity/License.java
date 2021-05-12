cc4492bcc847f88f5af4efefc71aa7da122b5729
dahui <ekgml3765@naver.com>
2021-05-12 17:24:06 +0900

    [backend] fix : 엔터티 변경
    [backend] fix : 엔터티 변경

--- backend/checksource/src/main/java/com/ssafy/checksource/model/entity/License.java
+++ backend/checksource/src/main/java/com/ssafy/checksource/model/entity/License.java
@@ -6,9 +6,12 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -65,4 +68,7 @@ public class License {
 	@OneToMany(mappedBy = "license", cascade = CascadeType.ALL)
     private List<LicenseOpensource> opensources = new ArrayList<>();
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
 }
