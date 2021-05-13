e54fa92b6e5e5b37003fe98a222d2c585ff0720b
dahui <ekgml3765@naver.com>
2021-05-13 09:36:51 +0900

    [backend] fix : 분석한 오픈소스 목록
    [backend] fix : 분석한 오픈소스 목록

--- backend/checksource/src/main/java/com/ssafy/checksource/controller/AnalyzeController.java
+++ backend/checksource/src/main/java/com/ssafy/checksource/controller/AnalyzeController.java
@@ -4,15 +4,12 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.checksource.model.dto.AnalyzeDTO;
-import com.ssafy.checksource.model.dto.OpensourceDTO;
+import com.ssafy.checksource.model.dto.AnalyOpensourceListDTO;
 import com.ssafy.checksource.model.dto.ProjectLiceseListDTO;
 import com.ssafy.checksource.model.dto.ProjectListByDepartDTO;
 import com.ssafy.checksource.service.AnalyzeService;
@@ -38,7 +35,7 @@ public class AnalyzeController {
 	
 	@ApiOperation(value = "분석된 프로젝트의 오픈소스 목록")
 	@GetMapping("/opensourceList")
-	public List<OpensourceDTO> getOpensourceListByProject(@RequestHeader("TOKEN") String token, @RequestParam String projectId) {
+	public AnalyOpensourceListDTO getOpensourceListByProject(@RequestHeader("TOKEN") String token, @RequestParam String projectId) {
 		return projectService.getOpensourceListByProject(projectId);
 	}
 	
--- backend/checksource/src/main/java/com/ssafy/checksource/model/repository/UnmappedOpensourceRepository.java
+++ backend/checksource/src/main/java/com/ssafy/checksource/model/repository/UnmappedOpensourceRepository.java
@@ -1,12 +1,15 @@
 package com.ssafy.checksource.model.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.ssafy.checksource.model.entity.Project;
 import com.ssafy.checksource.model.entity.UnmappedOpensource;
 
 
 public interface UnmappedOpensourceRepository extends JpaRepository<UnmappedOpensource, Long>{
 
-	
+	List<UnmappedOpensource> findByProject (Project project);
 }
