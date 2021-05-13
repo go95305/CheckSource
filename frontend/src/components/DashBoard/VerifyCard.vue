809c0c7f9571aa7988e14e3a502f6cfaa5dba2eb
NamWoojin <nam990304@gmail.com>
2021-05-13 11:53:50 +0900

    [frontend] feat : 검증된 프로젝트 axios 붙이기

--- frontend/src/components/DashBoard/VerifyCard.vue
+++ frontend/src/components/DashBoard/VerifyCard.vue
@@ -1,65 +1,72 @@
 <template>
-	<div class="verifycard">
-		<!-- left -->
-		<div class="verifycard-aside">
-			<div class="verifycard-aside-detail">
-				<span class="verifycard-difficulty">12</span><br />
-				<span class="verifycard-skill">Components</span>
-			</div>
+    <div class="verifycard">
+        <!-- left -->
+        <div class="verifycard-aside">
+            <div class="verifycard-aside-detail">
+                <span class="verifycard-difficulty">{{
+                    project.opensourceCnt
+                }}</span
+                ><br />
+                <span class="verifycard-skill">Components</span>
+            </div>
 
-			<div class="verifycard-detail">
-				<span class="verifycard-difficulty">5</span><br />
-				<span class="verifycard-skill">Licenses</span>
-			</div>
-		</div>
-		<!-- end of left -->
+            <div class="verifycard-detail">
+                <span class="verifycard-difficulty">{{
+                    project.licenseCnt
+                }}</span
+                ><br />
+                <span class="verifycard-skill">Licenses</span>
+            </div>
+        </div>
+        <!-- end of left -->
 
-		<!-- right -->
-		<div class="verifycard-main">
-			<div class="verifycard-main-first">
-				<p class="verifycard-excerpt">gitlab#1</p>
-				<h3 class="verifycard-title">Project Name</h3>
-			</div>
+        <!-- right -->
+        <div class="verifycard-main">
+            <div class="verifycard-main-first">
+                <p class="verifycard-excerpt">git</p>
+                <h3 class="verifycard-title">{{ project.name }}</h3>
+                <p>{{ project.branch }}</p>
+            </div>
 
-			<div class="verifycard-main-second">
-				<div class="verifycard-skill-s">resolving</div>
-			</div>
+            <div class="verifycard-main-second">
+                <div class="verifycard-skill-s">{{ project.status }}</div>
+            </div>
 
-			<div class="verifycard-main-third">
-				<div class="verifycard-excerpt">Last Scanned</div>
-				<div class="verifycard-excerpt-s">김신한</div>
-				<div class="verifycard-excerpt-s">2021.04.17</div>
-			</div>
+            <div class="verifycard-main-third">
+                <div class="verifycard-excerpt">Last Scanned</div>
+                <div class="verifycard-excerpt-s">{{ project.username }}</div>
+                <div class="verifycard-excerpt-s">{{ project.date }}</div>
+            </div>
 
-			<!-- down -->
-			<div class="verifycard-meta">
-				<div class="verifycard-btn">
-					<span class="material-icons"> replay </span>
-					<span class="vc-btn-detail">rescan</span>
-				</div>
+            <!-- down -->
+            <div class="verifycard-meta">
+                <div class="verifycard-btn">
+                    <span class="material-icons"> replay </span>
+                    <span class="vc-btn-detail">rescan</span>
+                </div>
 
-				<div class="verifycard-btn" @click="GoReport">
-					<span class="material-icons"> text_snippet </span>
-					<span class="vc-btn-detail">report</span>
-				</div>
-			</div>
-			<!-- end of down -->
-		</div>
-		<!-- end of right -->
-	</div>
+                <div class="verifycard-btn" @click="GoReport">
+                    <span class="material-icons"> text_snippet </span>
+                    <span class="vc-btn-detail">report</span>
+                </div>
+            </div>
+            <!-- end of down -->
+        </div>
+        <!-- end of right -->
+    </div>
 </template>
 
 <script>
 export default {
-	name: "VerifyCard",
-	props: {
-		project: Object,
-	},
-	methods: {
-		GoReport: function () {
-			this.$emit("goReport", "26342734");
-		},
-	},
+    name: "VerifyCard",
+    props: {
+        project: Object,
+    },
+    methods: {
+        GoReport: function () {
+            this.$emit("goReport", this.project.projectId);
+        },
+    },
 };
 </script>
 <style scoped src="@/assets/css/DashBoard/VerifyCard.css"></style>
