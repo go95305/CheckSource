80e65e9f96f1e18d9f83b1a8dd7e618a87997907
eunsu27 <pesu1027@gmail.com>
2021-05-14 23:29:51 +0900

    [frontend] feat : 라이선스 detail 전문, 기본정보 axios

--- frontend/src/components/OSS/OSSDetailLicenceTable.vue
+++ frontend/src/components/OSS/OSSDetailLicenceTable.vue
@@ -1,33 +1,40 @@
 <template>
-	<table class="detail-table">
-		<tr class="detail-table-tr">
-			<td class="detail-table-td-head">FULL NAME</td>
-			<td class="detail-table-td-body">이름</td>
-		</tr>
+  <table class="detail-table">
+    <tr class="detail-table-tr">
+      <td class="detail-table-td-head">FULL NAME</td>
+      <td class="detail-table-td-body">{{ license.name }}</td>
+    </tr>
 
-		<tr class="detail-table-tr">
-			<td class="detail-table-td-head">IDENTIFIER</td>
-			<td class="detail-table-td-body">라이선스</td>
-		</tr>
-		<tr class="detail-table-tr">
-			<td class="detail-table-td-head">CODE OPEN</td>
-			<td class="detail-table-td-body">
-				<span class="material-icons"> done </span>
-			</td>
-		</tr>
-		<tr class="detail-table-tr">
-			<td class="detail-table-td-head">URL</td>
-			<td class="detail-table-td-body">url</td>
-		</tr>
-	</table>
+    <tr class="detail-table-tr">
+      <td class="detail-table-td-head">IDENTIFIER</td>
+      <td class="detail-table-td-body">{{ license.identifier }}</td>
+    </tr>
+    <tr class="detail-table-tr">
+      <td class="detail-table-td-head">CODE OPEN</td>
+      <td class="detail-table-td-body" v-if="license.sourceopen.length != 0">
+        {{ license.sourceopen }}
+      </td>
+      <td class="detail-table-td-body" v-else>
+        <span class="material-icons"> close </span>
+      </td>
+      <!-- <td class="detail-table-td-body">
+        {{ license.sourceopen }}
+        <span class="material-icons"> done </span>
+      </td> -->
+    </tr>
+    <tr class="detail-table-tr">
+      <td class="detail-table-td-head">URL</td>
+      <td class="detail-table-td-body">{{ license.url }}</td>
+    </tr>
+  </table>
 </template>
 <script>
-import "@/assets/css/OSS/OSSDetailTable.css";
+import '@/assets/css/OSS/OSSDetailTable.css';
 export default {
-	name: "OSSDetailLicenseTable",
-	props: {
-		license: Object,
-	},
+  name: 'OSSDetailLicenseTable',
+  props: {
+    license: Object,
+  },
 };
 </script>
 <style scoped src="@/assets/css/OSS/OSSDetailLicenseTable.css"></style>
