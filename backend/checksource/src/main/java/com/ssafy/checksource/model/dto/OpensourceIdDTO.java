b655a529e39671e5a1805b1ae696380e905917a8
윤승환 <lovelyunsh@naver.com>
2021-05-15 21:57:20 +0900

    [backend] feat : delete mapping에 오픈소스, 라이선스 ID Json형태 변경 DTO

---
+++ backend/checksource/src/main/java/com/ssafy/checksource/model/dto/OpensourceIdDTO.java
@@ -0,0 +1,14 @@
+package com.ssafy.checksource.model.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class OpensourceIdDTO {
+	
+	private long opensourceId;
+
+
+}
+
