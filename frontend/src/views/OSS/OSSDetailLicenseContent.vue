ca8d4e098c530eb8376c5b881798e039cf516f91
NamWoojin <nam990304@gmail.com>
2021-05-17 22:39:45 +0900

    [frontend] feat : 디코딩 추가

--- frontend/src/views/OSS/OSSDetailLicenseContent.vue
+++ frontend/src/views/OSS/OSSDetailLicenseContent.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<div class="oss-license-content-div">
-			<p>{{ informationData.contents }}</p>
+			<p>{{ EncordingContent }}</p>
 		</div>
 	</div>
 </template>
@@ -10,6 +10,13 @@ export default {
 	name: "OSSDetailLicenseContent",
 	props: {
 		informationData: Object,
+	},
+	computed: {
+		EncordingContent: function () {
+			if (this.informationData.userName == "관리자")
+				return window.atob(this.informationData.contents);
+			else return this.informationData.contents;
+		},
 	},
 };
 </script>
