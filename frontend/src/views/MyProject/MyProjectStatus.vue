f35fc47c8960a17e4acf62779d32ab2bb5b51a30
eunsu27 <pesu1027@gmail.com>
2021-05-18 01:02:06 +0900

    [frontend] feat : 부서별 통계 - 카드형식

--- frontend/src/views/MyProject/MyProjectStatus.vue
+++ frontend/src/views/MyProject/MyProjectStatus.vue
@@ -10,6 +10,75 @@
     </div>
 
     <div class="status-body">
+      <div class="card status-graph">
+        <div class="card__header">
+          <div class="card__header-title">오픈소스 <strong>TOP5</strong></div>
+        </div>
+        <top-five-graph :labels="labels" :dataList="dataList"> </top-five-graph>
+      </div>
+
+      <!-- warning -->
+      <div class="card status-table-layout">
+        <div class="card__header">
+          <div class="card__header-title">
+            <strong>라이선스 의무 warning</strong>
+          </div>
+        </div>
+        <div id="chartdiv">
+          <div class="status-table-warning">
+            <div class="d-tbl-header">
+              <table
+                class="status-table"
+                cellpadding="0"
+                cellspacing="0"
+                border="0"
+              >
+                <caption>
+                  [코드공개(All) / 고지의무]
+                </caption>
+                <thead class="status-thead-css">
+                  <tr>
+                    <th scope="col">저장소</th>
+                    <th scope="col">부서</th>
+                    <th scope="col">프로젝트</th>
+                    <th scope="col">오픈소스</th>
+                    <th scope="col">라이선스</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+            <div class="s-tbl-content">
+              <table
+                class="status-table"
+                cellpadding="0"
+                cellspacing="0"
+                border="0"
+              >
+                <tbody>
+                  <tr>
+                    <td>gitlab#1</td>
+                    <td>ICT운영부</td>
+                    <td>ASC project</td>
+                    <td>Elasticsearch Extra Plugins</td>
+                    <td>GPL</td>
+                  </tr>
+                  <tr>
+                    <td>gitlab#3</td>
+                    <td>ICT운영부</td>
+                    <td>MinorLoan</td>
+                    <td>H2</td>
+                    <td>GPL</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--
+    <div class="status-body">
       <div class="status-graph">
         <top-five-graph :labels="labels" :dataList="dataList"> </top-five-graph>
       </div>
@@ -65,13 +134,13 @@
         </div>
       </div>
     </div>
+    -->
   </div>
 </template>
 <script>
 import MyProjectPath from '../../components/MyProject/MyProjectPath.vue';
 import TopFiveGraph from '@/components/DashBoard/TopFiveGraph.vue';
 import StatusOverview from '@/components/MyProject/StatusOverview.vue';
-import '@/assets/css/MyProject/MyProjectStatus.scss';
 import { mapGetters } from 'vuex';
 import dashboardApi from '@/api/dashboard.js';
 
@@ -147,3 +216,9 @@ export default {
   },
 };
 </script>
+
+<style
+  lang="scss"
+  scoped
+  src="@/assets/css/MyProject/MyProjectStatus.scss"
+></style>
