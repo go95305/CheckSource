a32982a6a05e92ced7e64484ab6dd1b531af2c7b
eunsu27 <pesu1027@gmail.com>
2021-05-18 10:23:06 +0900

    [frontend] fix : 부서별 통계 overview 툴팁 문구 추가

--- frontend/src/components/MyProject/StatusOverview.vue
+++ frontend/src/components/MyProject/StatusOverview.vue
@@ -6,7 +6,7 @@
         >프로젝트
       </div>
       <div class="tooltip-content">
-        <p>하잉 오늘은 화요일이예요</p>
+        <p class="tooltip-title">{{ msg.project }}</p>
       </div>
     </div>
     <div class="panel opensource">
@@ -14,17 +14,26 @@
         <span>{{ status.opensource }}</span
         >오픈소스
       </div>
+      <div class="tooltip-content">
+        <p class="tooltip-title">{{ msg.opensource }}</p>
+      </div>
     </div>
     <div class="panel license">
       <div class="status-title">
         <span>{{ status.license }}</span
         >라이선스
       </div>
+      <div class="tooltip-content">
+        <p class="tooltip-title">{{ msg.license }}</p>
+      </div>
     </div>
     <div class="panel warning">
       <div class="status-title">
         <span>{{ status.warning }}</span
         >주의
+      </div>
+      <div class="tooltip-content">
+        <p class="tooltip-title">{{ msg.warning }}</p>
       </div>
     </div>
   </div>
@@ -34,7 +43,14 @@
 export default {
   name: 'StatusOverview',
   data() {
-    return {};
+    return {
+      msg: {
+        project: '총 프로젝트 개수',
+        opensource: '사용된 오픈소스 종류의 개수',
+        license: '사용된 라이선스 종류의 개수',
+        warning: '사용 시 주의해야하는 라이선스 개수',
+      },
+    };
   },
   props: {
     status: Object,
