496bd9feefee7c0704f0f755ee5610e888298203
NamWoojin <nam990304@gmail.com>
2021-05-18 11:04:23 +0900

    [frontend] feat : 대시보드 의무 warning

--- frontend/src/api/dashboard.js
+++ frontend/src/api/dashboard.js
@@ -10,6 +10,17 @@ function readTopFive() {
 	return http.get(`/dashboard/top5`);
 }
 
+//전체 부서별 프로젝트 warning
+function readDashboardWarning(currentPage, size, time) {
+	return http.get(`/dashboard/warning`, {
+		params: {
+			currentPage: currentPage,
+			size: size,
+			time: time,
+		},
+	});
+}
+
 //부서별 통계 정보 불러오기 = 그래프
 function readTopFiveDepart(departId) {
 	return http.get(`/dashboard/top5/${departId}`);
@@ -20,9 +31,22 @@ function readDepartmentStatus(departId) {
 	return http.get(`/dashboard/statistics/${departId}`);
 }
 
+//부서별 통계 정보 = 부서별 프로젝트 warning
+function readDepartmentDashboardWarning(currentPage, departId, size, time) {
+	return http.get(`/dashboard/warning/${departId}`, {
+		params: {
+			currentPage: currentPage,
+			size: size,
+			time: time,
+		},
+	});
+}
+
 export default {
 	readStatistics,
 	readTopFive,
+	readDashboardWarning,
 	readTopFiveDepart,
 	readDepartmentStatus,
+	readDepartmentDashboardWarning,
 };
--- frontend/src/views/DashBoard/DashBoard.vue
+++ frontend/src/views/DashBoard/DashBoard.vue
@@ -1,139 +1,92 @@
 <template>
-    <div>
-        <div class="main__cards">
-            <!-- 저장소(부서) 별 정보 -->
-            <div class="depart-card card">
-                <div class="card__header">
-                    <div class="card__header-title">
-                        <strong>부서별 정보</strong>
-                    </div>
-                </div>
-                <!-- 총 갯수 -->
-                <div class="card__main">
-                    <!-- <DashBoardOverview /> -->
-                    <dash-board-overview :status="status">
-                    </dash-board-overview>
-                    <DashBoardTable
-                        :list="statisticsList"
-                        @getDepartProjects="GetDepartProjects"
-                    />
+	<div>
+		<div class="main__cards">
+			<!-- 저장소(부서) 별 정보 -->
+			<div class="depart-card card">
+				<div class="card__header">
+					<div class="card__header-title">
+						<strong>부서별 정보</strong>
+					</div>
+				</div>
+				<!-- 총 갯수 -->
+				<div class="card__main">
+					<!-- <DashBoardOverview /> -->
+					<dash-board-overview :status="status"> </dash-board-overview>
+					<DashBoardTable
+						:list="statisticsList"
+						@getDepartProjects="GetDepartProjects"
+					/>
 
-                    <div id="slider" v-if="projectList.length > 0">
-                        <div
-                            class="dash-c-btn dash-c-btn-prev"
-                            aria-label="Previous slide"
-                            @click="slide(-1)"
-                        >
-                            &#10094;
-                        </div>
-                        <transition-group
-                            tag="div"
-                            :name="transitionName"
-                            class="slides-group"
-                        >
-                            <div :key="current" class="slide">
-                                <VerifyCard
-                                    class="dashboard-verifycard"
-                                    :project="projectList[current]"
-                                />
-                            </div>
-                        </transition-group>
+					<div id="slider" v-if="projectList.length > 0">
+						<div
+							class="dash-c-btn dash-c-btn-prev"
+							aria-label="Previous slide"
+							@click="slide(-1)"
+						>
+							&#10094;
+						</div>
+						<transition-group
+							tag="div"
+							:name="transitionName"
+							class="slides-group"
+						>
+							<div :key="current" class="slide">
+								<VerifyCard
+									class="dashboard-verifycard"
+									:project="projectList[current]"
+								/>
+							</div>
+						</transition-group>
 
-                        <div
-                            class="dash-c-btn dash-c-btn-next"
-                            aria-label="Next slide"
-                            @click="slide(1)"
-                        >
-                            &#10095;
-                        </div>
-                    </div>
-                </div>
-            </div>
+						<div
+							class="dash-c-btn dash-c-btn-next"
+							aria-label="Next slide"
+							@click="slide(1)"
+						>
+							&#10095;
+						</div>
+					</div>
+				</div>
+			</div>
 
-            <!-- top5 -->
-            <div class="topfive-card card">
-                <div class="card__header">
-                    <div class="card__header-title text-light">
-                        오픈소스 <strong>TOP5</strong>
-                    </div>
-                </div>
-                <div class="dash-dropdown">
-                    <DropDown
-                        :orderList="departList"
-                        @orderItemChange="GetTopFive"
-                    />
-                </div>
-                <top-five-graph
-                    :labels="topFiveLabels"
-                    :dataList="topFiveValues"
-                >
-                </top-five-graph>
-            </div>
+			<!-- top5 -->
+			<div class="topfive-card card">
+				<div class="card__header">
+					<div class="card__header-title text-light">
+						오픈소스 <strong>TOP5</strong>
+					</div>
+				</div>
+				<div class="dash-dropdown">
+					<DropDown
+						name="topfive"
+						:orderList="departList"
+						@topfiveorderItemChange="GetTopFive"
+					/>
+				</div>
+				<top-five-graph :labels="topFiveLabels" :dataList="topFiveValues">
+				</top-five-graph>
+			</div>
 
-            <!-- warning -->
-            <div class="warning-card card">
-                <div class="card__header">
-                    <div class="card__header-title text-light">
-                        <strong>라이선스 의무 warning</strong>
-                    </div>
-                </div>
-                <div id="chartdiv">
-                    <div class="dash-dropdown">
-                        <DropDown :orderList="departList" />
-                    </div>
-
-                    <div class="dashboard-table">
-                        <div class="d-tbl-header">
-                            <table
-                                class="dash-table"
-                                cellpadding="0"
-                                cellspacing="0"
-                                border="0"
-                            >
-                                <caption>
-                                    [코드공개(All) / 고지의무]
-                                </caption>
-                                <thead class="dash-thead-css">
-                                    <tr>
-                                        <th scope="col">저장소</th>
-                                        <th scope="col">부서</th>
-                                        <th scope="col">프로젝트</th>
-                                        <th scope="col">오픈소스</th>
-                                        <th scope="col">라이선스</th>
-                                    </tr>
-                                </thead>
-                            </table>
-                        </div>
-                        <div class="d-tbl-content">
-                            <table
-                                class="dash-table"
-                                cellpadding="0"
-                                cellspacing="0"
-                                border="0"
-                            >
-                                <tbody>
-                                    <tr>
-                                        <td>gitlab#1</td>
-                                        <td>ICT운영부</td>
-                                        <td>ASC project</td>
-                                        <td>Elasticsearch Extra Plugins</td>
-                                        <td>GPL</td>
-                                    </tr>
-                                    <tr>
-                                        <td>gitlab#3</td>
-                                        <td>ICT운영부</td>
-                                        <td>MinorLoan</td>
-                                        <td>H2</td>
-                                        <td>GPL</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+			<!-- warning -->
+			<div class="warning-card card">
+				<div class="card__header">
+					<div class="card__header-title text-light">
+						<strong>라이선스 의무 warning</strong>
+					</div>
+				</div>
+				<div class="dash-dropdown">
+					<DropDown
+						name="warning"
+						:orderList="departList"
+						@warningorderItemChange="SetDepartId"
+					/>
+				</div>
+				<license-warning-table
+					:departId="warningDepartId"
+				></license-warning-table>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -145,106 +98,114 @@ import TopFiveGraph from "@/components/DashBoard/TopFiveGraph.vue";
 import VerifyCard from "@/components/DashBoard/VerifyCard.vue";
 import DropDown from "@/components/DropDown/DropDown.vue";
 import DashBoardTable from "@/components/DashBoard/DashBoardTable.vue";
+import LicenseWarningTable from "../../components/DashBoard/LicenseWarningTable.vue";
 import DashBoardOverview from "@/components/DashBoard/DashBoardOverview.vue";
 import "vueperslides/dist/vueperslides.css";
 
 export default {
-    name: "DashBoard",
-    components: {
-        TopFiveGraph,
-        VerifyCard,
-        DropDown,
-        DashBoardTable,
-        DashBoardOverview,
-    },
-    data() {
-        return {
-            statisticsList: [],
-            choicedDepartId: -1,
-            currentTime: 0,
-            projectList: [],
-            topFiveLabels: [],
-            topFiveValues: [],
-            current: 0,
-            direction: 1,
-            transitionName: "fade",
-            show: false,
-            status: {
-                project: "31",
-                opensource: "29",
-                license: "7",
-                warning: "15",
-            },
-            departList: [],
-        };
-    },
-    created() {
-        this.departList = Info.GetDepartmentList().slice();
-        this.departList.unshift("전체");
-        this.GetStatistics();
-        this.GetTopFive(0);
-    },
-    methods: {
-        GetStatistics() {
-            //부서별 통계정보 조회
-            dashboardApi.readStatistics().then((response) => {
-                if (response.data) {
-                    this.statisticsList = response.data.statisticsList;
-                    this.status.project = response.data.totalProjectCnt;
-                    this.status.opensource = response.data.totalOpensourceCnt;
-                    this.status.license = response.data.totalLicenseCnt;
-                    this.status.warning = response.data.totalWarningCnt;
-                }
-            });
-        },
-        GetDepartProjects(departId) {
-            //클릭한 부서의 프로젝트 리스트 조회
-            this.choicedDepartId = departId;
-            this.currentTime = dayjs().format("YYYY-MM-DD HH:mm:ss");
-            verifyApi
-                .readVerifiedProjectList(1, departId, "", 100, this.currentTime)
-                .then((response) => {
-                    this.projectList = response.data.projectList;
-                });
-        },
-        GetTopFive(index) {
-            //top5 정보 조회
-            console.log(index);
-            if (index == 0) {
-                dashboardApi.readTopFive().then((response) => {
-                    this.SetTopFive(response.data);
-                });
-            } else {
-                dashboardApi.readTopFiveDepart(index).then((response) => {
-                    this.SetTopFive(response.data);
-                    // console.log(this.topFiveList);
-                });
-            }
-        },
-        SetTopFive(list) {
-            let labels = [];
-            let values = [];
-            console.log(list);
-            for (let item of list) {
-                labels.push(item.name);
-                values.push(item.cnt);
-            }
-            this.topFiveLabels = labels;
-            this.topFiveValues = values;
-        },
-        slide(dir) {
-            console.log(this.current);
-            this.direction = dir;
-            dir === 1
-                ? (this.transitionName = "slide-next")
-                : (this.transitionName = "slide-prev");
-            var len = this.projectList.length;
-            this.current = (this.current + (dir % len) + len) % len;
-        },
-    },
-    mounted() {
-        this.show = true;
-    },
+	name: "DashBoard",
+	components: {
+		TopFiveGraph,
+		VerifyCard,
+		DropDown,
+		DashBoardTable,
+		DashBoardOverview,
+		LicenseWarningTable,
+	},
+	data() {
+		return {
+			statisticsList: [],
+			choicedDepartId: -1,
+			currentTime: 0,
+			projectList: [],
+			topFiveLabels: [],
+			topFiveValues: [],
+			warningDepartId: 0,
+			current: 0,
+			direction: 1,
+			transitionName: "fade",
+			show: false,
+			status: {
+				project: "31",
+				opensource: "29",
+				license: "7",
+				warning: "15",
+			},
+			departList: [],
+		};
+	},
+	created() {
+		this.departList = Info.GetDepartmentList().slice();
+		this.departList.unshift("전체");
+		this.GetStatistics();
+		this.GetTopFive(0);
+	},
+	methods: {
+		GetStatistics() {
+			//부서별 통계정보 조회
+			dashboardApi.readStatistics().then((response) => {
+				if (response.data) {
+					this.statisticsList = response.data.statisticsList;
+					this.status.project = response.data.totalProjectCnt;
+					this.status.opensource = response.data.totalOpensourceCnt;
+					this.status.license = response.data.totalLicenseCnt;
+					this.status.warning = response.data.totalWarningCnt;
+				}
+			});
+		},
+		GetDepartProjects(departId) {
+			//클릭한 부서의 프로젝트 리스트 조회
+			this.choicedDepartId = departId;
+			this.currentTime = dayjs().format("YYYY-MM-DD HH:mm:ss");
+			verifyApi
+				.readVerifiedProjectList(1, departId, "", 100, this.currentTime)
+				.then((response) => {
+					this.projectList = response.data.projectList;
+				});
+		},
+		GetTopFive(index) {
+			//top5 정보 조회
+			console.log(index);
+			if (index == 0) {
+				dashboardApi.readTopFive().then((response) => {
+					this.SetTopFive(response.data);
+				});
+			} else {
+				dashboardApi.readTopFiveDepart(index).then((response) => {
+					this.SetTopFive(response.data);
+					// console.log(this.topFiveList);
+				});
+			}
+		},
+		SetTopFive(list) {
+			let labels = [];
+			let values = [];
+			console.log(list);
+			for (let item of list) {
+				labels.push(item.name);
+				values.push(item.cnt);
+			}
+			this.topFiveLabels = labels;
+			this.topFiveValues = values;
+		},
+		SetDepartId(index) {
+			this.warningDepartId = index;
+		},
+		slide(dir) {
+			console.log(this.current);
+			this.direction = dir;
+			dir === 1
+				? (this.transitionName = "slide-next")
+				: (this.transitionName = "slide-prev");
+			var len = this.projectList.length;
+			this.current = (this.current + (dir % len) + len) % len;
+		},
+	},
+	mounted() {
+		this.show = true;
+	},
 };
 </script>
-<style lang="scss" scoped src="@/assets/css/DashBoard/DashBoard.scss"></style>
+<style lang="scss" scoped>
+@import "@/assets/css/DashBoard/DashBoard.scss";
+</style>
