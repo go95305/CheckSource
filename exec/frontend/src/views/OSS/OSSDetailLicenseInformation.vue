fdc003fe249ab0556c4725627b8ecb4aff834740
NamWoojin <nam990304@gmail.com>
2021-05-26 11:16:58 +0900

    [frontend] fix : 라이선스 상세 페이지 수정

--- exec/frontend/src/views/OSS/OSSDetailLicenseInformation.vue
+++ exec/frontend/src/views/OSS/OSSDetailLicenseInformation.vue
@@ -22,7 +22,7 @@
 </template>
 <script>
 export default {
-  name: 'OSSDetailLicenseInformation',
+  name: "OSSDetailLicenseInformation",
   props: {
     informationData: Object,
   },
@@ -31,52 +31,36 @@ export default {
       licenseId: this.$route.query.id,
       informationList: [
         {
-          name: 'OSI 승인여부',
-          value: 'this.informationData.is_osi',
+          name: "OSI 승인여부",
+          value: "this.informationData.is_osi",
         },
         {
-          name: 'Copyright 고지 여부',
-          value: 'this.informationData.is_copyright',
+          name: "저작권 고지 여부",
+          value: "this.informationData.is_copyright",
         },
         {
-          name: '라이선스 고지 여부',
-          value: 'this.informationData.is_license',
-        },
-        // {
-        //   name: '상업적 사용 가능 여부',
-        //   value: 'this.informationData.is_commercial',
-        // },
-        {
-          name: '수정 가능 여부',
-          value: 'this.informationData.is_modify',
-        },
-        // {
-        //   name: '비공개 사용 가능 여부',
-        //   value: 'this.informationData.is_private',
-        // },
-        // {
-        //   name: '특허권 사용 여부',
-        //   value: 'this.informationData.is_patent',
-        // },
-        {
-          name: '책임 여부',
-          value: 'this.informationData.is_respon',
+          name: "라이선스 사본 첨부 여부",
+          value: "this.informationData.is_license",
         },
         {
-          name: '보증 여부',
-          value: 'this.informationData.is_warranty',
-        },
-        // {
-        //   name: '동일한 라이선스 유지 여부',
-        //   value: 'this.informationData.is_continue',
-        // },
-        {
-          name: '배포 가능 여부',
-          value: 'this.informationData.is_distribute',
+          name: "수정 가능 여부",
+          value: "this.informationData.is_modify",
         },
         {
-          name: '상태 변경 시 문서화 여부',
-          value: 'this.informationData.is_status',
+          name: "타 라이선스 배포 허용",
+          value: "this.informationData.is_autherlicense",
+        },
+        {
+          name: "수정여부 고지 의무",
+          value: "this.informationData.is_modify_notice",
+        },
+        {
+          name: "책임 여부",
+          value: "this.informationData.is_respon",
+        },
+        {
+          name: "보증 여부",
+          value: "this.informationData.is_warranty",
         },
       ],
     };
