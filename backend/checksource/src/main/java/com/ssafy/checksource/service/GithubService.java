5e29cb208935bc3326ac8f153e18e819c817ed8c
KYC <go95305@naver.com>
2021-05-31 10:42:31 +0900

    [backend] fix: gitconnect error fix

--- backend/checksource/src/main/java/com/ssafy/checksource/service/GithubService.java
+++ backend/checksource/src/main/java/com/ssafy/checksource/service/GithubService.java
@@ -57,7 +57,7 @@ public class GithubService {
             return gitHubConnectDTO;
         } else {
             HttpHeaders headers = new HttpHeaders();
-            String plainCreds = "go95305:" + personalAccessToken;
+            String plainCreds = username + ":" + personalAccessToken;
             byte[] plainCredsBytes = plainCreds.getBytes();
             byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
             String base64Creds = new String(base64CredsBytes);
@@ -103,7 +103,7 @@ public class GithubService {
         // 헤더 담음
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        String plainCreds = username+":" + personalAccessToken;
+        String plainCreds = "go95305:" + personalAccessToken;
         byte[] plainCredsBytes = plainCreds.getBytes();
         byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
         String base64Creds = new String(base64CredsBytes);
